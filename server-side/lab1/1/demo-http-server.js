4fed9f84bd8ab720ae64f612103e5c128be56ca0
pisitponjanton <pisitponjan223@gmail.com>
2026-07-09 13:31:03 +0700

    Add /about and /contact routes

--- server-side/lab1/1/demo-http-server.js
+++ server-side/lab1/1/demo-http-server.js
@@ -6,6 +6,14 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello, World!\n");
+
+  if (req.url === "/about") {
+    res.end("Acout Page\n");
+  } else if (req.url === "/contact") {
+    res.end("Contact Page\n");
+  } else {
+    res.end("Hello, world!\n");
+  }
 });
 
 server.listen(port, hostname, () => {
